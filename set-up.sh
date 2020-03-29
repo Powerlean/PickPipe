6f34c2a2252053072e0b1dabd35d791aaf1f84eb
Xiao <bluebreeze191@gmail.com>
2020-03-29 14:01:43 +0800

    SERF

--- set-up.sh
+++ set-up.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 mv pipe /data/data/com.termux/files/usr/bin
 chmod +x /data/data/com.termux/files/usr/bin/pipe
-echo "安装完成"
+echo "* 安装完成"
 echo "你可以通过运行pipe h来查看使用方法">README.md
 echo "欢迎加入QQ群组进行讨论:902795006" >>README.md
 echo "或者你可以通过我的博客来联系我:edges5352.github.io" >>README.md
 cat README.md
-echo "正在删除安装包"
+echo "* 正在删除安装包"
 rm -rf set-up.sh
 
