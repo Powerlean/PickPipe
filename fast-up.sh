eef923bbfd89f7abb5d234670d6b0a38c36046c1
Xiao <bluebreeze191@gmail.com>
2020-03-30 20:13:22 +0800

    SERF

--- fast-up.sh
+++ fast-up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "\033[32m * 开始安装 \033[0m"
 apt install wget -y
-wget https://raw.githubusercontent.com/EdgeS5352/PickPipe/master/pipe
+wget https://cdn.jsdelivr.net/gh/EdgeS5352/PickPipe/pipe
 mv pipe $PREFIX/bin
 chmod +x $PREFIX/bin/pipe
 echo "\033[32m * 安装完成 \033[0m"
