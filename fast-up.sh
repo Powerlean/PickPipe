fab2bdc9cf946cb0fdcf4eed2cbc0f8bced0eedc
Xiao <bluebreeze191@gmail.com>
2020-04-01 11:26:40 +0800

    SERF

--- fast-up.sh
+++ fast-up.sh
@@ -4,7 +4,6 @@ apt install wget -y
 wget https://raw.githubusercontent.com/EdgeS5352/PickPipe/master/pipe
 mv pipe $PREFIX/bin
 chmod +x $PREFIX/bin/pipe
-clear
 echo -e "\033[32m * 安装完成 \033[0m"
 pipe
 echo -e "\033[32m * 删除安装包 \033[0m"
