1dcf255364438fc5e08961844f056d346e2a1b1f
Xiao <bluebreeze191@gmail.com>
2020-04-14 10:34:40 +0800

    SERF(stable)

--- fast-up.sh
+++ fast-up.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo -e "\033[32m ↻ 移除旧软件包 \033[0m"
+rm -rf /data/data/com.termux/files/usr/bin/pipe
+rm -rf /data/data/com.termux/files/usr/bin pipe.*
 echo -e "\033[32m ↻ 开始安装 \033[0m"
 apt install wget -y
 wget https://raw.githubusercontent.com/EdgeS5352/PickPipe/master/pipe
